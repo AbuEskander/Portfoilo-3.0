422acdfc2dbe609d1222286bdd88e101e8fa23e0
Basil Ismail <basselawni1@gmail.com>
2024-02-12 22:12:52 +0300

    Fixing headr showing wrong name

---
+++ Client/porto/src/Components/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>اتصل بي</h1>
+      <p>البريد الالكتروني:</p>
+    </div>
+  );
+};
+
+export default Contact;
--- Client/porto/src/index.js
+++ Client/porto/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./Components/App.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Projects from "./Components/Projects";
+import Projects from "./Components/Projects.js";
 import Header from "./Components/Header.js";
-
+import Contact from "./Components/Contact.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
@@ -15,6 +15,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
